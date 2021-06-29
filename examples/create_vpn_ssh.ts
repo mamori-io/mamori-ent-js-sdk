a72cfbd9ad7ca584f172cb44304b4ee6161e1af6
Peter Maher <pmaher@mamori.io>
2021-06-29 12:24:22 +1000

    [ch3934] Add key

--- examples/create_vpn_ssh.ts
+++ examples/create_vpn_ssh.ts
@@ -9,34 +9,24 @@
 import { ExampleWrapper } from './example_wrapper' ;
 import { DMService } from '../dist/api';
 import { ParsedArgs } from 'minimist';
-
-let usage: string =       
-"Usage:\n" + 
-"   yarn ts-node <example script> [--help] [--url <url>] [<user> <password>] vpnName -L local_port:desination:desination_port user@ssh_server\n" + 
-"where:\n" + 
-"   user\t\tmamori server user\n" +
-"   password\n" +
-"   url\t\tDefault: localhost:443\n" + 
-"   vpnName\t\tVPN name\n" + 
-"   ssh_server\tThe remote SSH server IP address\n" + 
-"   user\t\tSpecifies the user to log in as on the remote machine\n" + 
-"   local_port\tThe local machine port \n" + 
-"   desination\tThe IP or hostname of the destination machine\n" + 
-"   desination_port\tThe port of the destination machine" ;
+import * as fs from "fs";
+import { promisify } from "util";
 
 let eg = async function (dm: DMService, args: ParsedArgs) {
   let vpnName = args._[2] ;
 
-  let sshsrv: string[] = args._[3].split('@') ;
-  let user = sshsrv[0] ;
-  let server = sshsrv[1] ;
+  let keyFile = args.i ;
 
   let tunnel: string[] = args.L.split(':') ;
-  let local_port = tunnel[0] ;
-  let destination = tunnel[1] ;
+  let local_port       = tunnel[0] ;
+  let destination      = tunnel[1] ;
   let destination_port = tunnel[2] ;
 
-  console.info(`VPN: ${vpnName} -L ${local_port}:${destination}:${destination_port} ${user}@${server}`);
+  let sshsrv: string[] = args._[3].split('@') ;
+  let user   = sshsrv[0] ;
+  let server = sshsrv[1] ;
+
+  console.info(`VPN: ${vpnName} -i ${keyFile} -L ${local_port}:${destination}:${destination_port} ${user}@${server}`);
 
   try {
     await dm.destroy_vpn_connection(vpnName) 
@@ -46,21 +36,38 @@ let eg = async function (dm: DMService, args: ParsedArgs) {
     console.info("Delete VPN: ", (e as Error).message);
   }
 
+  let privateKey: string = await promisify(fs.readFile)(keyFile, { encoding: "UTF-8" }) ;
+  //console.info(privateKey);
+
   await dm.create_vpn_connection({
     name: vpnName, 
     type: "ssh", 
     config: {host: server, port: 22, username: user, localPort: local_port, remoteHost:destination, remotePort: destination_port},
-    secrets: {privateKey: "", publicKey: ""}
+    secrets: [{name: "privateKey", value: privateKey}]
   });
 }
 
 let rapt = new ExampleWrapper(eg, process.argv, {
-  string: ['url', '-L'],
+  string: ['url', '-i', '-L'],
   alias: { h: 'help' },
   default: { url: 'localhost:443' },
   '--': true,
 }) ;
-rapt.usage = usage ;
+rapt.usage =
+"Usage:\n" + 
+"   yarn ts-node <example script> [--help] [--url <url>] [<user> <password>] vpnName  -i identity_file -L local_port:desination:desination_port user@ssh_server\n" + 
+"where:\n" + 
+"   user\t\tmamori server user\n" +
+"   password\n" +
+"   url\t\tDefault: localhost:443\n" + 
+"   vpnName\t\tVPN name\n" + 
+"   identity_file\t\tFile containing the private key\n" + 
+"   ssh_server\tThe remote SSH server IP address\n" + 
+"   user\t\tSpecifies the user to log in as on the remote machine\n" + 
+"   local_port\tThe local machine port \n" + 
+"   desination\tThe IP or hostname of the destination machine\n" + 
+"   desination_port\tThe port of the destination machine" ;
+
 rapt.execute()
     .catch((e: any) => console.error("ERROR: ", e.response == undefined ? e : e.response.data))
     .finally(() => process.exit(0));
