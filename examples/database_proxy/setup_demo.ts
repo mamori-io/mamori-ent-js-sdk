85b044fde46d48212cdf0bc070ca7a84175ea00e
Peter Maher <pmaher@mamori.io>
2021-08-17 14:51:34 +1000

    [ch3934] WIP

---
+++ examples/database_proxy/setup_demo.ts
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2021 mamori.io.  All Rights Reserved.
+ *
+ * This software contains the confidential and proprietary information of mamori.io.
+ * Parties accessing this software are required to maintain the confidentiality of all such information.
+ * mamori.io reserves all rights to this software and no rights and/or licenses are granted to any party
+ * unless a separate, written license is agreed to and signed by mamori.io.
+ */
+import { ExampleWrapper } from '../example_wrapper' ;
+import { DMService } from '../../dist/api';
+import { ParsedArgs } from 'minimist';
+  
+let revealRoleName: string  = "data_reveal";
+let endorseRoleName: string = "data_endorser";
+let userRoleName: string    = "data_user";
+
+let projectionName: string = "view_customer_details";
+let accessName: string = "data_access";
+
+let usage: string =       
+"Usage:\n" + 
+"   yarn ts-node --transpile-only examples/database_proxy/setup_demo.ts [--help] [--url <url>] [<user> <password>] [<system>]\n" + 
+"where:\n" + 
+"   user      mamori server user\n" +
+"   password\n" +
+"   url       Default: localhost:443\n" +
+"   system    Default: oracle\n" +
+"\n" +
+"After using this script to setup the data demonstration, a user with the data_user role can request access:\n\n" +
+"   yarn ts-node --transpile-only examples/request_workflow/access_request.ts [--help] [--url <url>] [<user> <password>] data_access [<message>] [--time=<N>]\n" + 
+"   \n" +
+"This will output a request key, e.g. 03048fd3-017b-0c52-399a-00001660cb1e.\n" +
+"The request can be endorsed or denied by a user with the data_endorser role, or cancelled by the requester using:\n\n" +
+"   yarn ts-node --transpile-only examples/request_workflow/access_action.ts [--help] --url <url> <user> <action> <requestKey> [<message>]\n" +
+"   \n" +
+"If endorsed, the requester will be granted the data_reveal role temporarily, revealing the masked data for N seconds.\n" ;
+
+let eg = async function (dm: DMService, args: ParsedArgs) {
+  //
+  // Roles
+  //
+  var endorseRole = await dm.role(endorseRoleName);
+  if (endorseRole) {
+    console.info("Endorser role: ", endorseRoleName);
+  }
+  else {
+    endorseRole = await dm.create_role({ roleid: endorseRoleName });
+    console.info("Created role: ", endorseRoleName);
+    await dm.grant_to(endorseRoleName, ['REQUEST'], "*", false) ;
+  }
+
+  var userRole = await dm.role(userRoleName);
+  if (userRole) {
+    console.info("User role: ", userRoleName);
+  }
+  else {
+    userRole = await dm.create_role({ roleid: userRoleName });
+    console.info("Created role: ", userRoleName);
+  }
+
+  var revealRole = await dm.role(revealRoleName);
+  if (revealRole) {
+    console.info("Reveal role: ", revealRoleName);
+  }
+  else {
+    revealRole = await dm.create_role({ roleid: revealRoleName });
+    console.info("Created role: ", revealRoleName);
+  }
+
+  //
+  //  Access policy 
+  //
+  let system = args._[2] || "oracle" ;
+
+  // Setup anew
+  await dm.policies_set_policy_projection(system + ".orcl.DEMO.customer_pii", "CREDIT_CARD_NO", "MASKED BY credit_card()", "default", null) ;
+  await dm.policies_set_policy_projection(system + ".orcl.DEMO.customer_pii", "*", "REVEAL", projectionName, null) ;
+  console.info("Created data projection: ", projectionName);
+
+  await dm.policies_create_procedure(accessName,
+    {a: {name: "time", description: "Duration of access", default_value: "30"}},
+    endorseRoleName,
+    "policy",
+    "Grant access to customer data",
+    userRoleName,
+    "",
+    "",
+    "",
+    "",
+    "1",
+    "",
+    "true",
+    "",
+    "BEGIN; GRANT " + revealRoleName + " TO :applicant VALID FOR :time seconds; END");
+  console.info("Created access policy: ", accessName);
+
+  var projectionResult = await dm.get_http_apifilters({ filter: ["name", "=", projectionName] });
+  if (projectionResult) {
+    await dm.activate_http_apifilter(projectionResult.data[0].id);
+    console.info("Activated filter: ", projectionName);
+  }
+}
+
+let rapt = new ExampleWrapper(eg, process.argv) ;
+rapt.usage = usage ;
+rapt.execute()
+    .catch((e: any) => console.error("ERROR: ", e.response == undefined ? e : e.response.data))
+    .finally(() => process.exit(0));
---
+++ examples/ssh_proxy/setup_demo.ts
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2021 mamori.io.  All Rights Reserved.
+ *
+ * This software contains the confidential and proprietary information of mamori.io.
+ * Parties accessing this software are required to maintain the confidentiality of all such information.
+ * mamori.io reserves all rights to this software and no rights and/or licenses are granted to any party
+ * unless a separate, written license is agreed to and signed by mamori.io.
+ */
+import { ExampleWrapper } from '../example_wrapper' ;
+import { DMService } from '../../dist/api';
+import { ParsedArgs } from 'minimist';
+
+let mgrRoleName = "appp_manager";
+let userRoleName = "appp_user";
+let endorseRoleName = "appp_endorser";
+let filterName = "appp_customers";
+let accessName = "appp_access";
+
+let eg = async function (dm: DMService, args: ParsedArgs) {
+  //
+  // appp roles
+  //
+  var mgrRole = await dm.role(mgrRoleName);
+  if (mgrRole) {
+    console.info("Manager role: ", mgrRoleName);
+  }
+  else {
+    mgrRole = await dm.create_role({ roleid: mgrRoleName });
+    console.info("Created role: ", mgrRoleName);
+  }
+
+  var userRole = await dm.role(userRoleName);
+  if (userRole) {
+    console.info("User role: ", userRoleName);
+  }
+  else {
+    userRole = await dm.create_role({ roleid: userRoleName });
+    console.info("Created role: ", userRoleName);
+  }
+
+  var endorseRole = await dm.role(endorseRoleName);
+  if (endorseRole) {
+    console.info("Endorser role: ", endorseRoleName);
+  }
+  else {
+    endorseRole = await dm.create_role({ roleid: endorseRoleName });
+    console.info("Created role: ", endorseRoleName);
+    await dm.grant_to(endorseRoleName, ['REQUEST'], "*", false) ;
+  }
+
+  //
+  //  apppsense filter
+  //
+
+  // Teardown existing
+  //
+  var filterResult = await dm.get_http_apifilters([["name", "=", filterName]]);
+  if (filterResult.data && filterResult.data.length > 0) {
+    await dm.delete_http_apifilter(filterResult.data[0].id);
+    console.info("Deleted filter: ", filterName);
+  }
+  await dm.policies_drop_procedure(accessName);
+  console.info("Deleted procedure: ", accessName);
+
+  // Setup anew
+  //
+  await dm.add_http_apifilter({
+    name: filterName,
+    system: "appp",
+    type: "appp",
+    path: "Customer Detail",
+    method: "",
+    queryparameters: "",
+    headers: "",
+    body: "",
+    owner: args._[0],
+    transformations: 
+      '[{"name":"default","priority":1,"function":"MASK HASH","elementSpec":"Customer Name","functionArgs":"MD5"},' +
+      '{"name":"default","priority":1,"function":"MASK FULL","elementSpec":"Customer Gender"},' +
+      '{"name":"default","priority":1,"function":"MASK FULL","elementSpec":"Sales Revenue (Current Year)","functionArgs":"9|$,"},' +
+      '{"name":"appp_manager","priority":1,"function":"REVEAL","elementSpec":"*"}]',
+  });
+  console.info("Created appp filter: ", filterName);
+
+  await dm.policies_create_procedure(accessName,
+    {a: {name: "time", description: "Duration of access", default_value: "30"}},
+    endorseRoleName,
+    "policy",
+    "Grant access to appp data",
+    userRoleName,
+    "",
+    "",
+    "",
+    "",
+    "1",
+    "",
+    "true",
+    "",
+    "BEGIN; GRANT " + mgrRoleName + " TO :applicant VALID FOR :time seconds; END");
+  console.info("Created access policy: ", accessName);
+
+  var filterResult = await dm.get_http_apifilters({ filter: ["name", "=", filterName] });
+  if (filterResult) {
+    await dm.activate_http_apifilter(filterResult.data[0].id);
+    console.info("Activeted filter: ", filterName);
+  }
+}
+
+let rapt = new ExampleWrapper(eg, process.argv) ;
+rapt.execute()
+    .catch((e: any) => console.error("ERROR: ", e.response == undefined ? e : e.response.data))
+    .finally(() => process.exit(0));
