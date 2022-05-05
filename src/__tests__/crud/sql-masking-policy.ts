bcd9768827fd160178b44d9ab3258aa41a2b917a
omar masri <omar@masri.com.au>
2022-05-05 14:27:29 +1000

    Added test for db policy

--- src/__tests__/crud/sql-masking-policy.ts
+++ src/__tests__/crud/sql-masking-policy.ts
@@ -3,7 +3,7 @@ import * as https from 'https';
 import { SQLMaskingPolicy } from '../../sql-masking-policy';
 import { Role } from '../../role';
 import { User } from '../../user';
-import { MamoriPermission, MAMORI_PERMISSION } from '../../permission';
+import { MamoriPermission, MAMORI_PERMISSION, PolicyPermission } from '../../permission';
 import { handleAPIException, noThrow, ignoreError, FILTER_OPERATION } from '../../utils';
 
 const testbatch = process.env.MAMORI_TEST_BATCH || '';
@@ -61,22 +61,38 @@ describe("sql-masking-policy crud tests", () => {
         let x3 = await noThrow(SQLMaskingPolicy.get(api, name));
         expect(x3).toBeDefined();
 
+        let filter = [["permissiontype", FILTER_OPERATION.EQUALS_STRING, "POLICY"],
+        ["grantee", FILTER_OPERATION.EQUALS_STRING, grantee],
+        ["policy", FILTER_OPERATION.EQUALS_STRING, name]];
+        let res = await new PolicyPermission().grantee(grantee).list(api, filter);
+        expect(res.totalCount).toBe(0);
+
         //Grant a policy
         let x4 = await noThrow(o.grantTo(api, grantee));
         expect(x4.errors).toBe(false);
+        let res2 = await new PolicyPermission().grantee(grantee).list(api, filter);
+        expect(res2.totalCount).toBe(1);
         //Revoke a policy
         let x5 = await noThrow(o.revokeFrom(api, grantee));
         expect(x5.errors).toBe(false);
+        let res3 = await new PolicyPermission().grantee(grantee).list(api, filter);
+        expect(res3.totalCount).toBe(0);
 
 
         //Add masking rules
 
+        //GRANT AGAIN - to test delete removes permissions
+        let x6 = await noThrow(o.grantTo(api, grantee));
+        expect(x6.errors).toBe(false);
+
         //Delete Policy
         let d0 = await noThrow(o.delete(api));
         expect(d0.error).toBe(false);
+        let d2 = await noThrow(SQLMaskingPolicy.list(api, 0, 10, [["name", FILTER_OPERATION.EQUALS_STRING, o.name]]));
+        expect(d2.totalCount).toBe("0");
         //Check permissions gone
-
-
+        let res4 = await new PolicyPermission().grantee(grantee).list(api, filter);
+        expect(res4.totalCount).toBe(0);
         done();
     });
 
