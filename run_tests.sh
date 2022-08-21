f88b6cd000b4b0de01313c5e9c8883c093a72f90
omar masri <omar@masri.com.au>
2022-08-21 12:24:27 +1000

    Updated test scripts

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
+yarn test crud
 yarn test permissions
-yarn test crud/alert
-yarn test crud/datasource
-yarn test crud/encryption
-yarn test crud/ip-resource
-yarn test crud/mamori
-yarn test crud/network.ssh
-yarn test crud/on-demand
-yarn test crud/remote-desktop
-yarn test crud/role
-yarn test crud/sql-masking
-yarn test crud/ssh-login
-yarn test crud/wireguard-peer
+yarn test pam
