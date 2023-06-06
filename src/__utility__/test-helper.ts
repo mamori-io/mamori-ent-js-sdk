a599d894a099f61e4216d8f92d95dd2625af7c8e
Daniel Parnell <me@danielparnell.com>
2023-06-06 00:49:57 +0000

    Added a test to ensure the SSH tunnel is actually initialized

--- src/__utility__/test-helper.ts
+++ src/__utility__/test-helper.ts
@@ -2,6 +2,42 @@ import { assert } from "console";
 import { io_permission, io_serversession, io_sqlmaskingpolicies, MamoriService, io_role, io_ondemandpolicies, io_key } from "../api";
 import { io_utils, io_https } from "../api";
 
+const childProcess = require("child_process");
+
+/**
+ * @param {string} command A shell command to execute
+ * @return {Promise<string>} A promise that resolve to the output of the shell command, or an error
+ * @example const output = await execute("ls -alh");
+ */
+export function execute(command: string): Promise<string> {
+  /**
+   * @param {Function} resolve A function that resolves the promise
+   * @param {Function} reject A function that fails the promise
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+   */
+    return new Promise(function(resolve: any, reject: any) {
+    /**
+     * @param {Error} error An error triggered during the execution of the childProcess.exec command
+     * @param {string|Buffer} standardOutput The result of the shell command execution
+     * @param {string|Buffer} standardError The error resulting of the shell command execution
+     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+     */
+	childProcess.exec(command, function(error: any, standardOutput: any, standardError: any) {
+	    if (error) {
+		reject(error);
+
+		return;
+	    }
+
+	    resolve(standardOutput);
+	});
+  });
+}
+
+
+export function sleep(milliseconds: number) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
 
 export class DBHelper {
 
