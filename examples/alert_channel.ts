79f1d692b7ac7731db0cc4d4ed8f83cae9be2e27
omar masri <omar@masri.com.au>
2024-01-26 17:44:18 +1100

    Updated examples

--- examples/alert_channel.ts
+++ examples/alert_channel.ts
@@ -21,39 +21,42 @@ async function example() {
 	  	let login = await api.login(mamoriUser, mamoriPwd);
 	  	console.info("Login successful for: ", login.fullname, ", session: ", login.session_id);
 
-	  	console.info("creating alert channel...");
-	  	let name = "SOMENAME" ;
+	  	///////////////////
+		//CONFIGURE OBJECT
+		let name = "SOMENAME" ;
 		let k = new io_alertchannel.AlertChannel(name);
-		//Delete old one
+		k.addEmailAlert("omasri@mamori.io", "test subject", "My Message");
+		let body = {
+			"attachments": [
+				{
+					"color": "#f93836",
+					"pretext": "A wireguard peer has been blocked",
+					"author_name": "Mamori",
+					"title": "Wireguard peer blocked - {{device}}",
+					"text": "User: {{username}} - client ip: {{source}}",
+					"footer": "This is a Mamori Peer Notification",
+				}
+			]
+		};
+		k.addHTTPAlert(io_alertchannel.HTTP_OPERATION.POST
+			, ""
+			, "https://hooks.slack.com/services/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"
+			, JSON.stringify(body)
+			, "application/json");
+	  	k.addPushNotificationAlert("{{applicant}}", "Test Message");
+	  	////////////
+		//CREATE IT
+		let r = await io_utils.noThrow(k.create(api));
+		console.info("creating alert channel...%s %o",name,r);
+		////////////
+		//READ IT
 		let r2 = await io_utils.noThrow(io_alertchannel.AlertChannel.get(api, name));
-		if (r2) {
-			await io_utils.ignoreError(r2.delete(api));
-		}
-		//Set config for new one
-	  	k.addEmailAlert("omasri@mamori.io", "test subject", "My Message");
-	  	let body = {
-		  "attachments": [
-			  {
-				  "color": "#f93836",
-				  "pretext": "A wireguard peer has been blocked",
-				  "author_name": "Mamori",
-				  "title": "Wireguard peer blocked - {{device}}",
-				  "text": "User: {{username}} - client ip: {{source}}",
-				  "footer": "This is a Mamori Peer Notification",
-			  }
-		  ]
-	  };
-	  k.addHTTPAlert(io_alertchannel.HTTP_OPERATION.POST
-		, ""
-		, "https://hooks.slack.com/services/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"
-		, JSON.stringify(body)
-		, "application/json")
-	 
-	  k.addPushNotificationAlert("{{applicant}}", "Test Message");
-	  let r = await io_utils.noThrow(k.create(api));
-
-	  console.log("**** %o",r);
-	  api.logout();
+		console.info("reading alert channel...%o",r2);
+		////////////
+		//DELETE IT
+		await  io_utils.ignoreError(io_utils.noThrow(r2.delete(api)));
+	  	//
+		api.logout();
 }
 
 example()
--- examples/encryption-key.ts
+++ examples/encryption-key.ts
@@ -23,41 +23,7 @@ async function example() {
 	  	console.info("Login successful for: ", login.fullname, ", session: ", login.session_id);
 
 	  	console.info("creating connection policy...");
-	  	let name = "c-policy" ;
-		let cleanit = await io_utils.noThrow(io_alertchannel.AlertChannel.get(api, name));
-		if (cleanit.length > 0) {
-            let p2 = io_policy.ConnectionPolicy.build(cleanit[0]);
-            await io_utils.noThrow(p2.delete(api));
-        }
-
-		//Delete old one
-		let p2 = await io_utils.noThrow(io_alertchannel.AlertChannel.get(api, name));
-		if (p2) {
-			await io_utils.ignoreError(p2.delete(api));
-		}
-
-		//Set config for new one
-		let policy = new io_policy.ConnectionPolicy(io_policy.POLICY_TYPES.BEFORE_CONNECTION, name);
-        policy.withEnabled(false)
-            .withPosition(20)
-            .withEnabled(false)
-            .withAction(io_policy.POLICY_ACTIONS.ALLOW)
-            .withRuleType(io_policy.POLICY_RULE_TYPE.WHEN)
-            .withRuleSEXP("(SOURCE-IP 127.0.0.1)")
-            .withAlert(requestAlert.name);
-
-	  
-	  k.addHTTPAlert(io_alertchannel.HTTP_OPERATION.POST
-		, ""
-		, "https://hooks.slack.com/services/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"
-		, JSON.stringify(body)
-		, "application/json")
-	 
-	  k.addPushNotificationAlert("{{applicant}}", "Test Message");
-	  let r = await io_utils.noThrow(k.create(api));
-
-	  console.log("**** %o",r);
-	  api.logout();
+	  	
 }
 
 example()
