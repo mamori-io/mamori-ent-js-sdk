b382cc736d50391cdb6f0f501ba71657d66546e4
allegra <allegra@outlook.com>
2024-01-29 11:14:02 +0000

    Added encryption key example

--- examples/encryption-key.ts
+++ examples/encryption-key.ts
@@ -3,13 +3,17 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 import { lchmodSync } from 'fs';
 //import { MamoriService,io_https,io_utils } from 'mamori-ent-js-sdk';
 //import { } from 'mamori-ent-js-sdk';
-import { MamoriService,io_https, io_utils } from '../src/api';
-import { io_policy, io_alertchannel, io_role, io_permission, io_user } from "../src/api";
+import { MamoriService,io_https, io_key,io_utils } from '../src/api';
+import { io_role, io_permission } from "../src/api";
 
 
 const mamoriUrl = process.env.MAMORI_SERVER || '';
 const mamoriUser = process.env.MAMORI_USERNAME || '';
 const mamoriPwd = process.env.MAMORI_PASSWORD || '';
+const host = process.env.MAMORI_SERVER || '';
+const password = process.env.MAMORI_PASSWORD || '';
+const dbPassword = process.env.MAMORI_DB_PASSWORD || '';
+
 const INSECURE = new io_https.Agent({ rejectUnauthorized: false });
 
 //let mamoriUrl = "https://localhost/" ;
@@ -20,10 +24,39 @@ async function example() {
 	 	let api = new MamoriService(mamoriUrl);
 		console.info("Connecting...");
 	  	let login = await api.login(mamoriUser, mamoriPwd);
-	  	console.info("Login successful for: ", login.fullname, ", session: ", login.session_id);
 
-	  	console.info("creating connection policy...");
-	  	
+	  	console.info("Login successful for: ", login.fullname, ", session: ", login.session_id);
+	
+		//CREATE ROLE TO BE GRANTEE GRANTEE
+		let grantee = "example_Role_key";
+		let role = new io_role.Role(grantee);
+		await io_utils.noThrow(role.create(api));
+
+		//////////////
+		//CONFIGURE IT
+		let name = "example_rsa_key";
+		let k = new io_key.Key(name);
+		k.ofType(io_key.KEY_TYPE.AES);
+        
+		//Create
+        await io_utils.noThrow(k.create(api));
+        console.info("creating key...%s", name);
+
+		////////////
+		//READ IT
+		let x = (await io_utils.noThrow(io_key.Key.getAll(api))).find((key: any) => key.name == k.name);
+        console.info("reading key...%s %o", name,x);
+		///////////
+		// GRANT IT
+		await io_utils.noThrow(k.grantTo(api, grantee)); 
+		console.info("granting key...%s", name);
+		//////////
+		//DELETE IT
+		await io_utils.ignoreError(k.delete(api));
+		console.info("deleting key...%s", name);
+
+
+		
 }
 
 example()
