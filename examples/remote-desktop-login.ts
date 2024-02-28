ebe6f7d97c5dea7cd3a020bec458924c0e2c9be3
allegra <allegra@outlook.com>
2024-02-28 04:43:44 +0000

    examples added

---
+++ examples/remote-desktop-login.ts
@@ -0,0 +1,79 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+import { lchmodSync } from 'fs';
+//import { MamoriService,io_https,io_utils } from 'mamori-ent-js-sdk';
+//import { } from 'mamori-ent-js-sdk';
+import { MamoriService,io_https, io_utils, io_remotedesktop } from '../src/api';
+import * as helper from '../src/__utility__/test-helper';
+
+const mamoriUser = process.env.MAMORI_USERNAME || '';
+const mamoriPwd = process.env.MAMORI_PASSWORD || '';
+const INSECURE = new io_https.Agent({ rejectUnauthorized: false });
+const host = process.env.MAMORI_SERVER || '';
+
+function fail(reason = "fail was called in a test.") {
+  throw new Error(reason);
+}
+
+//let mamoriUrl = "https://localhost/" ;
+//let mamoriUser = "alice" ;
+//let mamoriPwd  = "mirror" ;
+
+async function example() {
+    let api = new MamoriService(host, INSECURE);
+    let apiAsAPIUser: MamoriService;
+		console.info("Connecting...");
+	  let login = await api.login(mamoriUser, mamoriPwd);
+	  console.info("Login successful for: ", login.fullname, ", session: ", login.session_id);
+   
+
+
+    /////////////////////////
+    // CREATE GRANTEE
+    let grantee = "example_apiuser_rmdlogin";
+    let granteepw = "J{J'vpKs!$nW6(6A,4!3#$4#12_vdQ'}D";
+    
+    await io_utils.ignoreError(api.delete_user(grantee));
+    await api.create_user({
+          username: grantee,
+          password: granteepw,
+          fullname: grantee,
+          identified_by: "password",
+          email: "example@example.example"
+        }).catch(e => {
+            fail(io_utils.handleAPIException(e));
+        })
+    apiAsAPIUser = new MamoriService(host, INSECURE);
+    await apiAsAPIUser.login(grantee, granteepw);
+
+    ///////////////
+    //CONFIGURE IT
+    let name: string = "example_remote_desktop_login";
+    let r = new io_remotedesktop.RemoteDesktopLogin("test_rdp_login", io_remotedesktop.REMOTE_DESKTOP_PROTOCOL.RDP);
+    r.at("host", "port").withLoginMode(io_remotedesktop.LOGIN_PROMPT_MODE.MAMORI_PROMPT);
+
+    /////////////
+    // CREATE IT
+    let k = r.fromJSON(r.toJSON());
+    await io_utils.noThrow(r.create(api));
+    console.info("creating remote_desktop_login...%s", name);
+    ///////////
+    //READ IT
+    await io_utils.noThrow(io_remotedesktop.RemoteDesktopLogin.getByName(api, k.name));
+    await io_utils.noThrow(io_remotedesktop.RemoteDesktopLogin.getByName(api, k.name));
+    await io_utils.noThrow(io_remotedesktop.RemoteDesktopLogin.list(apiAsAPIUser, 0, 10, [["name", "=", k.name]]));
+    console.info("reading remote_desktop_login...%s", name);
+
+    //////////
+    //GRANT IT
+    await io_utils.noThrow(k.grantTo(api, grantee));
+
+    ///////////
+    //DELETE IT
+    await io_utils.noThrow(r.delete(api));
+    console.info("deleting remote_desktop_login...%s", name);
+}
+
+example()
+  .catch(e => console.error("ERROR: ", e))
+  .finally(() => process.exit(0));
---
+++ examples/role.ts
@@ -0,0 +1,68 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+import { lchmodSync } from 'fs';
+//import { MamoriService,io_https,io_utils } from 'mamori-ent-js-sdk';
+//import { } from 'mamori-ent-js-sdk';
+import { MamoriService,io_https, io_utils, io_user} from '../src/api';
+import { ignoreError } from '../src/utils';
+import { Role } from '../src/api';
+
+const mamoriUser = process.env.MAMORI_USERNAME || '';
+const mamoriPwd = process.env.MAMORI_PASSWORD || '';
+const INSECURE = new io_https.Agent({ rejectUnauthorized: false });
+const host = process.env.MAMORI_SERVER || '';
+
+//let mamoriUrl = "https://localhost/" ;
+//let mamoriUser = "alice" ;
+//let mamoriPwd  = "mirror" ;
+
+async function example() {
+    let api = new MamoriService(host, INSECURE);
+		console.info("Connecting...");
+	  let login = await api.login(mamoriUser, mamoriPwd);
+	  console.info("Login successful for: ", login.fullname, ", session: ", login.session_id);
+    let apiAsAPIUser: MamoriService;
+
+    /////////////////////////
+    // CREATE ROLE TO GRANTEE
+    let grantee = "example_roles_user.";
+    let granteepw = "J{J'vpKs!$nW6(6A,4!98712_vdQ'}D";
+
+    await ignoreError(api.delete_user(grantee));
+    await api.create_user({
+      username: grantee,
+      password: granteepw,
+      fullname: grantee,
+      identified_by: "password",
+      email: "test@test.test"
+    })
+    apiAsAPIUser = new MamoriService(host, INSECURE);
+    await apiAsAPIUser.login(grantee, granteepw);
+    
+    ///////////////
+    //CONFIGURE IT
+    let name: string = "example_automated_role";
+    let r = new Role(name);
+
+    /////////////
+    // CREATE IT
+    await io_utils.noThrow(r.create(api));
+    console.info("creating role...%s", name);
+    ///////////
+    //READ IT
+    (await io_utils.noThrow(Role.getAll(api))).filter((o: any) => o.roleid == r.roleid);
+    console.info("reading role...%s", name);
+
+    ///////////
+    //GRANT IT
+    (await io_utils.noThrow(r.getGrantees(apiAsAPIUser)));
+    console.info("granting role...%s", name);
+    ///////////
+    //DELETE IT
+    await io_utils.noThrow(r.delete(api));
+    console.info("deleting role...%s", name);
+}
+
+example()
+  .catch(e => console.error("ERROR: ", e))
+  .finally(() => process.exit(0));
